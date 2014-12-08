75590e40a854d9f10fdde78e1480df1cac6074ae
Marcelo Sampaio <marcelo@artlogica.com.br>
2014-12-08 15:16:01 -0200

    Initial Commit

---
+++ SortDescriptor/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SortDescriptor
+//
+//  Created by Marcelo Sampaio on 12/8/14.
+//  Copyright (c) 2014 Marcelo Sampaio. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SortDescriptor/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SortDescriptor
+//
+//  Created by Marcelo Sampaio on 12/8/14.
+//  Copyright (c) 2014 Marcelo Sampaio. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
